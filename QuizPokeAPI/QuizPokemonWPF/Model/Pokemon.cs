68439a6eba32faefdcb2c7d2e1d48f5c267ef563
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-12-04 21:40:29 -0600

    Cambios

---
+++ QuizPokeAPI/QuizPokemonWPF/Model/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizPokemonWPF.Model
+{
+    public class Pokemon
+    {
+       public string url { get; set; }
+
+
+
+    }
+}
---
+++ QuizPokeAPI/QuizPokemonWPF/ViewModel/PokemonViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using QuizPokemonWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Text;
+
+namespace QuizPokemonWPF.ViewModel
+{
+    public class PokemonViewModel: INotifyPropertyChanged
+    {
+        HttpClient client = new HttpClient() { BaseAddress = new Uri("https://pokeapi.co/") };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<object> Pokemones { get; set; } = new ObservableCollection<object>();
+
+        public PokemonViewModel()
+        {
+            //GetPokemon();
+        }
+
+        //async void GetPokemon()
+        //{
+        //    var result = await client.GetAsync("/api/v2/characteristic");
+        //    if (result.IsSuccessStatusCode)
+        //    {
+        //        string datos = await result.Content.ReadAsStringAsync();
+        //        var lista = JsonConvert.DeserializeObject<List<Pokemon>>(datos);
+
+        //        Pokemones.Clear();
+
+        //        lista.ForEach(x => Pokemones.Add(x));
+        //    }
+        //}
+    }
+}
