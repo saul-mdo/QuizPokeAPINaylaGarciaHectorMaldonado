319ce2bc891c2977c52b69fb275b5368172783b7
Nayla Garcia <nayla.garcia@r2devpros.com>
2020-12-28 23:37:29 -0600

    Diseño agregado

---
+++ QuizPokeAPI/QuizPokemonWPF/PuntajeView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuizPokemonWPF
+{
+    /// <summary>
+    /// Lógica de interacción para PuntajeView.xaml
+    /// </summary>
+    public partial class PuntajeView : Window
+    {
+        public PuntajeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QuizPokeAPI/QuizPokemonWPF/Views/InicioUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizPokemonWPF.Views
+{
+    /// <summary>
+    /// Lógica de interacción para InicioUserControl.xaml
+    /// </summary>
+    public partial class InicioUserControl : UserControl
+    {
+        public InicioUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
