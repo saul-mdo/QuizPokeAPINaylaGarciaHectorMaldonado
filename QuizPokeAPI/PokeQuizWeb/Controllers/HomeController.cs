17fad3ad96ecace5150a3232a129c64d19b1bc0c
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2021-01-06 15:58:54 -0600

    Web, gnera 20 pokemon

--- QuizPokeAPI/PokeQuizWeb/Controllers/HomeController.cs
+++ QuizPokeAPI/PokeQuizWeb/Controllers/HomeController.cs
@@ -24,25 +24,7 @@ namespace PokeQuizWeb.Controllers
         {
             try
             {
-
-                //traer lo pokemones
-                //var i = 0;
-                //while (i < 20)
-                //{
-                //    var id = IdPokemonRandom();
-                //    vm.ListaPokemones.Add(await GetPokemon(id));
-                //    vm.ListaPokemones = await GetPokemones();
-                //    i++;
-                //}
-                //ViewBag.Texto = vm.ListaPokemones[0].name.ToString();
-                //llenar las preguntas
-                //LLenarPreguntas(vm);
-
-
-              await GetPokemon();
-
-               var i = 3;
-
+              await LlenarListaPokemon();
 
             }
             catch (Exception ex)
@@ -54,50 +36,7 @@ namespace PokeQuizWeb.Controllers
 
         }
 
-        private void LLenarPreguntas(QuizViewModel vm)
-        {
-            //pregunta1
-            //vm.P1.TextoPregunta = vm.ListaPokemones[6].types[6].Name;
-            //vm.P1.Opcion1 = vm.ListaPokemones[7].name;
-            //vm.P1.Opcion2 = vm.ListaPokemones[1].name;
-            //vm.P1.Opcion3 = vm.ListaPokemones[9].name;
-            //vm.P1.OpcionRespuesta = vm.ListaPokemones[6].name;
-            ////pregunta2
-
-        }
-
-        public int IdPokemonRandom()
-        {
-            Random r = new Random();
-            int idRandom = r.Next(1, 898);
-            return idRandom;
-        }
-
-        [HttpPost]
-        //public IActionResult Index(QuizViewModel vm)
-        //{
-            
-        //    try
-        //    {
-
-        //        //se trae las respuestas
-        //        //se comparan con las respuesta correcta
-        //        //se muestran los resultados
-
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        ModelState.AddModelError("", ex.Message);
-
-
-        //    }
-
-        //    return RedirectToAction("Puntaje");
-
-        //}
-
-
+        
 
         public async Task<IActionResult> Quiz()
         {
@@ -114,51 +53,49 @@ namespace PokeQuizWeb.Controllers
             return View();
         }
 
-       
 
-        private async Task<Pokemon> GetPokemon()
+        public int IdPokemonRandom()
+        {
+            Random r = new Random();
+            int idRandom = r.Next(1, 898);
+            return idRandom;
+        }
+
+        private async Task<Pokemon> LlenarListaPokemon()
         {
             QuizViewModel vm = new QuizViewModel();
             vm.ListaPokemones = new List<Pokemon>();
-            var id = IdPokemonRandom();
-            c = Factory.CreateClient("pokemones");
-            var response = await c.GetAsync($"api/v2/pokemon/{id}");
+            var i = 0;
+            while (i < 20)
+            {
+                var id = IdPokemonRandom();
+                c = Factory.CreateClient("pokemones");
+                var response = await c.GetAsync($"api/v2/pokemon/{id}");
 
-            if (response.IsSuccessStatusCode)
-            {
-                var json = await response.Content.ReadAsStringAsync();
-                var p = JsonConvert.DeserializeObject<Pokemon>(json);
-                // var p = JsonConvert.DeserializeObject<Pokemon>(json);
-                vm.ListaPokemones.Add(p);
-                //return x;
-                return p;
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var p = JsonConvert.DeserializeObject<Pokemon>(json);
+                    vm.ListaPokemones.Add(p);
+                    i++;
+                }
             }
-            else
-            {
-                return null;
-            }
+
+            return null;
 
         }
 
-        //private async Task<List<Pokemon>> GetPokemones()
-        //{
+        private void LLenarPreguntas(QuizViewModel vm)
+        {
+            //pregunta1
+            //vm.P1.TextoPregunta = vm.ListaPokemones[6].types[6].Name;
+            //vm.P1.Opcion1 = vm.ListaPokemones[7].name;
+            //vm.P1.Opcion2 = vm.ListaPokemones[1].name;
+            //vm.P1.Opcion3 = vm.ListaPokemones[9].name;
+            //vm.P1.OpcionRespuesta = vm.ListaPokemones[6].name;
+            ////pregunta2
 
-        //    c = Factory.CreateClient("pokemones");
-        //    var response = await c.GetAsync("api/v2/pokemon/?limit=20&offset=20");
-
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        var json = await response.Content.ReadAsStringAsync();
-        //        var Pokemons = JsonConvert.DeserializeObject<List<Pokemon>>(json);
-        //        return Pokemons;
-
-
-        //    }
-
-        //    return null;
-        //}
-
-
+        }
 
     }
 }
