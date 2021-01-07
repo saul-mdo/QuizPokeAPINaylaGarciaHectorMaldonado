a1e4304aa2c6db16f889a6f5b6fe13a7411c62eb
Nayla Garcia <nayla.garcia@r2devpros.com>
2021-01-06 18:20:29 -0600

    muestra ya todos

--- QuizPokeAPI/PokeQuizWeb/Controllers/HomeController.cs
+++ QuizPokeAPI/PokeQuizWeb/Controllers/HomeController.cs
@@ -43,12 +43,73 @@ namespace PokeQuizWeb.Controllers
         {
             QuizViewModel vm = new QuizViewModel();
             vm.ListaPokemones = await LlenarListaPokemon();
-
+            //LLENAR LAS PREGUNTAS
+            //Pregunta 1
            vm.P1.TextoPregunta = vm.ListaPokemones[0].name;
             vm.P1.Opcion1 = vm.ListaPokemones[7].name;
             vm.P1.Opcion2 = vm.ListaPokemones[1].name;
             vm.P1.Opcion3 = vm.ListaPokemones[9].name;
             vm.P1.OpcionRespuesta = vm.ListaPokemones[6].name;
+            //Pregunta 2
+            vm.P2.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P2.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P2.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P2.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P2.OpcionRespuesta = vm.ListaPokemones[6].name;
+
+            //Pregunta 3
+            vm.P3.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P3.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P3.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P3.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P3.OpcionRespuesta = vm.ListaPokemones[6].name;
+
+            //pregunta 4
+            vm.P4.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P4.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P4.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P4.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P4.OpcionRespuesta = vm.ListaPokemones[6].name;
+            //pregunta 5
+            vm.P5.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P5.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P5.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P5.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P5.OpcionRespuesta = vm.ListaPokemones[6].name;
+
+            //pregunta 6
+            vm.P6.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P6.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P6.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P6.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P6.OpcionRespuesta = vm.ListaPokemones[6].name;
+            //pregunta 7
+            vm.P7.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P7.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P7.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P7.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P7.OpcionRespuesta = vm.ListaPokemones[6].name;
+
+            //pregunta 8
+            vm.P8.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P8.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P8.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P8.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P8.OpcionRespuesta = vm.ListaPokemones[6].name;
+
+            //pregunta 9
+            vm.P9.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P9.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P9.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P9.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P9.OpcionRespuesta = vm.ListaPokemones[6].name;
+
+            //pregunta 10
+            vm.P10.TextoPregunta = vm.ListaPokemones[0].name;
+            vm.P10.Opcion1 = vm.ListaPokemones[7].name;
+            vm.P10.Opcion2 = vm.ListaPokemones[1].name;
+            vm.P10.Opcion3 = vm.ListaPokemones[9].name;
+            vm.P10.OpcionRespuesta = vm.ListaPokemones[6].name;
             return View(vm);
         }
 
